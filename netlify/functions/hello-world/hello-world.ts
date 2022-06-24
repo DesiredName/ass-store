144504e640669e7ae69eaec5e9324e75b7643315
DesiredName <wc3-enter@yandex.ru>
2022-06-24 15:57:38 +0200

    update ravendb code

--- netlify/functions/hello-world/hello-world.ts
+++ netlify/functions/hello-world/hello-world.ts
@@ -1,11 +1,16 @@
 import { Handler } from '@netlify/functions'
 import { DocumentStore } from 'ravendb'
+import * as fs from 'fs'
 
-const store = new DocumentStore('https://a.free.ass-store.ravendb.cloud', 'test');
+const store = new DocumentStore('https://a.free.ass-store.ravendb.cloud', 'test', {
+  certificate: fs.readFileSync('./certificate.pfx'),
+  password: '4FE45E96448CCD2D830D5C1E98A306D',
+  type: 'pfx'
+});
 store.initialize();
 
 export const handler: Handler = async (event, context) => {
-  const { name = 'iPhone X' } = event.queryStringParameters || {}
+  const { name = 'iPhone X' + new Date() } = event.queryStringParameters || {}
   
   const product = {
     id: name,
@@ -19,7 +24,7 @@ export const handler: Handler = async (event, context) => {
   };
 
   const session = store.openSession();
-  await session.store(product, 'products/');
+  await session.store(product, `products/${name}`);
   await session.saveChanges();
 
   const result = await session.load(`products/${name}`);
